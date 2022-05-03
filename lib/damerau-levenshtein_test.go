8d39850be4db72d17552af7561ba139350d48698
xztaityozx <taityonohibi@gmail.com>
2022-05-03 16:43:11 +0900

    test: remove unnecessary test

--- lib/damerau-levenshtein_test.go
+++ lib/damerau-levenshtein_test.go
@@ -7,68 +7,66 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-
 func TestNewDamerauLevenshtein(t *testing.T) {
-  type args struct {
-    replaceCost int
-    swapCost int
-    isLimited bool
-  }
+	type args struct {
+		replaceCost int
+		swapCost    int
+		isLimited   bool
+	}
 
-  tests := []struct {
-    name string
-    args args
-    expect DamerauLevenshtein 
-  } {
-    {name: "replaceCost 10, limited", args: args{replaceCost: 10, swapCost: 11, isLimited: true}, expect: DamerauLevenshtein{Levenshtein: Levenshtein{ReplaceCost: 10},   SwapCost: 11, Limited: true}},
-    {name: "replaceCost 10, unlimited", args: args{replaceCost: 10, swapCost: 11, isLimited: false}, expect: DamerauLevenshtein{Levenshtein: Levenshtein{ReplaceCost: 10},SwapCost: 11, Limited: false}},
-  }
+	tests := []struct {
+		name   string
+		args   args
+		expect DamerauLevenshtein
+	}{
+		{name: "replaceCost 10, limited", args: args{replaceCost: 10, swapCost: 11, isLimited: true}, expect: DamerauLevenshtein{Levenshtein: Levenshtein{ReplaceCost: 10}, SwapCost: 11, Limited: true}},
+		{name: "replaceCost 10, unlimited", args: args{replaceCost: 10, swapCost: 11, isLimited: false}, expect: DamerauLevenshtein{Levenshtein: Levenshtein{ReplaceCost: 10}, SwapCost: 11, Limited: false}},
+	}
 
-  for _, tt := range tests {
-    actual := NewDamerauLevenshtein(tt.args.replaceCost, tt.args.swapCost, tt.args.isLimited)
-    t.Run(tt.name, func(t *testing.T) {
-      assert.Equal(t, actual, tt.expect)
-    })
-  }
+	for _, tt := range tests {
+		actual := NewDamerauLevenshtein(tt.args.replaceCost, tt.args.swapCost, tt.args.isLimited)
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expect, actual)
+		})
+	}
 }
 
 func TestDamerauLevenshtein_Walk(t *testing.T) {
-  type args struct {
-    replaceCost int
-    swapCost int
-    limited bool
-    a string
-    b string
-  }
+	type args struct {
+		replaceCost int
+		swapCost    int
+		limited     bool
+		a           string
+		b           string
+	}
 
-  tests := []struct{
-    args args
-    expect int
-    hasError bool
-  } {
-    {args: args{replaceCost: 1,swapCost: 1, limited: true, a: "str", b: "stu"}, expect: 1, hasError: false},
-    {args: args{replaceCost: 1, swapCost: 1, limited: true, a: "ca", b: "abc"}, expect: 3, hasError: false},
-    {args: args{replaceCost: 1,swapCost: 2, limited: true, a: "str", b: "stu"}, expect: 1, hasError: false},
-    {args: args{replaceCost: 1, swapCost: 2, limited: true, a: "ca", b: "abc"}, expect: 3, hasError: false},
-    {args: args{replaceCost: 1, swapCost: 1, limited: true, a: "acb", b: "abc"}, expect: 1, hasError: false},
-    {args: args{replaceCost: 1, swapCost: 3, limited: true, a: "acb", b: "abc"}, expect: 2, hasError: false},
+	tests := []struct {
+		args     args
+		expect   int
+		hasError bool
+	}{
+		{args: args{replaceCost: 1, swapCost: 1, limited: true, a: "str", b: "stu"}, expect: 1, hasError: false},
+		{args: args{replaceCost: 1, swapCost: 1, limited: true, a: "ca", b: "abc"}, expect: 3, hasError: false},
+		{args: args{replaceCost: 1, swapCost: 2, limited: true, a: "str", b: "stu"}, expect: 1, hasError: false},
+		{args: args{replaceCost: 1, swapCost: 2, limited: true, a: "ca", b: "abc"}, expect: 3, hasError: false},
+		{args: args{replaceCost: 1, swapCost: 1, limited: true, a: "acb", b: "abc"}, expect: 1, hasError: false},
+		{args: args{replaceCost: 1, swapCost: 3, limited: true, a: "acb", b: "abc"}, expect: 2, hasError: false},
 
-    {args: args{replaceCost: 1,swapCost: 1, limited: false, a: "str", b: "stu"}, expect: 1, hasError: false},
-    {args: args{replaceCost: 1, swapCost: 1, limited: false, a: "ca", b: "abc"}, expect: 3, hasError: false},
-    {args: args{replaceCost: 1,swapCost: 2, limited: false, a: "str", b: "stu"}, expect: 1, hasError: false},
-    {args: args{replaceCost: 1, swapCost: 1, limited: false, a: "ca", b: "abc"}, expect: 2, hasError: false},
-    {args: args{replaceCost: 1, swapCost: 1, limited: false, a: "acb", b: "abc"}, expect: 1, hasError: false},
-    {args: args{replaceCost: 1, swapCost: 3, limited: false, a: "acb", b: "abc"}, expect: 2, hasError: false},
-  }
+		{args: args{replaceCost: 1, swapCost: 1, limited: false, a: "str", b: "stu"}, expect: 1, hasError: false},
+		{args: args{replaceCost: 1, swapCost: 2, limited: false, a: "str", b: "stu"}, expect: 1, hasError: false},
+		{args: args{replaceCost: 1, swapCost: 1, limited: false, a: "ca", b: "abc"}, expect: 2, hasError: false},
+		{args: args{replaceCost: 1, swapCost: 1, limited: false, a: "acb", b: "abc"}, expect: 1, hasError: false},
+		{args: args{replaceCost: 1, swapCost: 3, limited: false, a: "acb", b: "abc"}, expect: 2, hasError: false},
+	}
 
-  for _, v := range tests {
-    t.Run(fmt.Sprintf("%s <-> %s cost=(%v,%v) limited: %v", v.args.a, v.args.b, v.args.replaceCost, v.args.swapCost, v.args.limited), func(t *testing.T) {
-      actual, err := NewDamerauLevenshtein(v.args.replaceCost,v.args.swapCost, v.args.limited).Walk(v.args.a, v.args.b)
-      if (err != nil) != v.hasError {
-        assert.Errorf(t, err, "DamerauLevenshtein.Walk() hasError = %v", v.hasError)
-        return
-      }
-      assert.Equal(t, actual, v.expect)
-    })
-  }
+	for _, v := range tests {
+		t.Run(fmt.Sprintf("%s <-> %s cost=(%v,%v) limited: %v", v.args.a, v.args.b, v.args.replaceCost, v.args.swapCost, v.args.limited), func(t *testing.T) {
+			actual, err := NewDamerauLevenshtein(v.args.replaceCost, v.args.swapCost, v.args.limited).Walk(v.args.a, v.args.b)
+			if (err != nil) != v.hasError {
+				assert.Errorf(t, err, "DamerauLevenshtein.Walk() hasError = %v", v.hasError)
+				return
+			}
+			assert.Equal(t, v.expect, actual)
+		})
+	}
 }
--- lib/levenshtein_test.go
+++ lib/levenshtein_test.go
@@ -1,57 +1,57 @@
 package lib
 
 import (
-  "reflect"
-  "testing"
+	"reflect"
+	"testing"
 
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewLevenshtein(t *testing.T) {
-  cost := 10
-  expect := Levenshtein{ReplaceCost: cost}
-  actual := NewLevenshtein(cost)
-  assert.Equal(t, expect, actual)
+	cost := 10
+	expect := Levenshtein{ReplaceCost: cost}
+	actual := NewLevenshtein(cost)
+	assert.Equal(t, expect, actual)
 }
 
 func TestLevenshtein_Walk(t *testing.T) {
-  type arg struct {
-    a string
-    b string
-  }
-  tests := []struct{
-    name string
-    arg arg
-    expect int
-    cost int
-    hasError bool
-  } {
-    {name: "str <-> stu", arg: arg{a: "str", b:"stu"}, expect:1, cost: 1, hasError:false },
-    {name: "str <-> stir", arg: arg{a: "str", b:"stir"}, expect:1, cost: 1, hasError:false },
-    {name: "kitten <-> sitting", arg: arg{a: "kitten", b:"sitting"}, expect:3, cost: 1, hasError:false },
-    {name: "str <-> abc", arg: arg{a: "str", b:"abc"}, expect:3, cost: 1, hasError:false },
-    {name: "str <-> str", arg: arg{a: "str", b:"str"}, expect:0, cost: 1, hasError:false },
-    {name: "str <-> ", arg: arg{a: "str", b:""}, expect:3, cost: 1, hasError:false },
+	type arg struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name     string
+		arg      arg
+		expect   int
+		cost     int
+		hasError bool
+	}{
+		{name: "str <-> stu", arg: arg{a: "str", b: "stu"}, expect: 1, cost: 1, hasError: false},
+		{name: "str <-> stir", arg: arg{a: "str", b: "stir"}, expect: 1, cost: 1, hasError: false},
+		{name: "kitten <-> sitting", arg: arg{a: "kitten", b: "sitting"}, expect: 3, cost: 1, hasError: false},
+		{name: "str <-> abc", arg: arg{a: "str", b: "abc"}, expect: 3, cost: 1, hasError: false},
+		{name: "str <-> str", arg: arg{a: "str", b: "str"}, expect: 0, cost: 1, hasError: false},
+		{name: "str <-> ", arg: arg{a: "str", b: ""}, expect: 3, cost: 1, hasError: false},
 
-    {name: "str <-> stu", arg: arg{a: "str", b:"stu"}, expect:2, cost: 2, hasError:false },
-    {name: "str <-> stir", arg: arg{a: "str", b:"stir"}, expect:1, cost: 2, hasError:false },
-    {name: "kitten <-> sitting", arg: arg{a: "kitten", b:"sitting"}, expect:5, cost: 2, hasError:false },
-    {name: "str <-> abc", arg: arg{a: "str", b:"abc"}, expect:6, cost: 2, hasError:false },
-    {name: "str <-> str", arg: arg{a: "str", b:"str"}, expect:0, cost: 2, hasError:false },
-    {name: "str <-> ", arg: arg{a: "str", b:""}, expect:3, cost: 2, hasError:false },
-  }
+		{name: "str <-> stu", arg: arg{a: "str", b: "stu"}, expect: 2, cost: 2, hasError: false},
+		{name: "str <-> stir", arg: arg{a: "str", b: "stir"}, expect: 1, cost: 2, hasError: false},
+		{name: "kitten <-> sitting", arg: arg{a: "kitten", b: "sitting"}, expect: 5, cost: 2, hasError: false},
+		{name: "str <-> abc", arg: arg{a: "str", b: "abc"}, expect: 6, cost: 2, hasError: false},
+		{name: "str <-> str", arg: arg{a: "str", b: "str"}, expect: 0, cost: 2, hasError: false},
+		{name: "str <-> ", arg: arg{a: "str", b: ""}, expect: 3, cost: 2, hasError: false},
+	}
 
-  for _, tt := range tests {
-    t.Run(tt.name, func(t *testing.T) {
-      actual, err := Levenshtein{ReplaceCost: tt.cost}.Walk(tt.arg.a, tt.arg.b)
-      if (err != nil) != tt.hasError {
-        t.Errorf("Levenshtein.Walk() want error = %v, hasError = %v", err, tt.hasError)
-        return
-      }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := Levenshtein{ReplaceCost: tt.cost}.Walk(tt.arg.a, tt.arg.b)
+			if (err != nil) != tt.hasError {
+				t.Errorf("Levenshtein.Walk() want error = %v, hasError = %v", err, tt.hasError)
+				return
+			}
 
-      if !reflect.DeepEqual(actual, tt.expect) {
-        t.Errorf("Levenshtein.Walk() actual = %v expect = %v", actual, tt.expect)
-      }
-    })
-  }
+			if !reflect.DeepEqual(actual, tt.expect) {
+				t.Errorf("Levenshtein.Walk() actual = %v expect = %v", actual, tt.expect)
+			}
+		})
+	}
 }
