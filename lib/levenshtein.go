ba653ff4b5dffafe9fe5f4896bba0cca4b883372
xztaityozx <taityonohibi@gmail.com>
2022-05-03 15:40:10 +0900

    chore: add createTable

--- lib/levenshtein.go
+++ lib/levenshtein.go
@@ -13,17 +13,7 @@ func NewLevenshtein(cost int) Levenshtein {
 func (l Levenshtein) Walk(a, b string) (int, error) {
   aLen := len(a)
   bLen := len(b)
-  var dp = make([][]int, aLen+1)
-  for i := 0;i<aLen+1;i++ {
-    dp[i] = make([]int, bLen+1)
-  }
-
-  for i:=0;i<aLen+1;i++ {
-    dp[i][0] = i;
-  }
-  for i:=0;i<bLen+1;i++ {
-    dp[0][i] = i;
-  }
+  dp := createTable(aLen, bLen)
 
   for i := 1; i<= aLen; i++ {
     for k := 1; k<= bLen; k++ {
